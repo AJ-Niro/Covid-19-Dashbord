4fa9dd24ffc05d8d557de93b61949fc8d161e5f5
AJ-Niro <andresninorodil@gmail.com>
2025-09-07 20:02:45 -0400

    feat: make basic endpoint to get top countries cases data

---
+++ src/app/api/covid/topCountries/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { DISEASE_API_URL } from "@src/config/environmentVariables";
+
+export type CountryData = {
+  country: string;
+  cases: number;
+  [key: string]: unknown;
+};
+
+export async function GET() {
+  const res = await fetch(`${DISEASE_API_URL}/countries`);
+  const json: CountryData[] = await res.json();
+
+  const top10 = json
+    .sort((a, b) => b.cases - a.cases)
+    .slice(0, 10)
+    .map((c) => ({
+      country: c.country,
+      cases: c.cases,
+    }));
+
+  return NextResponse.json(top10);
+}
